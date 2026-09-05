8788b20bd52c3866b15f627d03180eeb69729a1e
zklosko <31484611+zklosko@users.noreply.github.com>
2026-09-05 12:52:18 -0400

    adding TcpTransport export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { MockDevice } from "./device/MockDevice.js"
 export { UdpTransport } from "./transport/UdpTransport.js"
+export { TcpTransport } from "./transport/TcpTransport.js"
 export type { Transport, TransportClient } from "./transport/Transport.js"
