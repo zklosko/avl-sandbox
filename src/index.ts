6e81ed1e8b722ca69e56980388a8017e6912dc4c
zklosko <31484611+zklosko@users.noreply.github.com>
2026-09-04 21:42:46 -0400

    corrected exports

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,3 @@
-import { MockDevice } from "./MockDevice.js";
-import { UdpTransport } from "./UdpTransport.js";
-
-const projector = new MockDevice(
-    new UdpTransport({port: 4352})
-)
-
-projector.defineState("power", false)
-
-projector.command("PWR ON", (device) => {
-    device.setState("power", true)
-    return "PWR ON"
-})
-
-projector.command("PWR OFF", (device) => {
-    device.setState("power", false)
-    return "PWR OFF"
-})
-
-projector.command("PWR?", (device) => {
-    return device.getState<boolean>("power") ? "PWR ON" : "PWR OFF"
-})
-
-await projector.start()
-console.log("Projector listening on UDP port 4352")
+export { MockDevice } from "./MockDevice.js"
+export { UdpTransport } from "./transport/UdpTransport.js"
+export type { Transport, TransportClient } from "./transport/Transport.js"
